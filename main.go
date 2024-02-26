e311ddb7aa84b77662001688970d3e06ca2dfe1c
Tomas Vilemaitis <tomas.vilemaitis@gmail.com>
2024-02-26 18:04:31 +0200

    fix SA1017 error

--- main.go
+++ main.go
@@ -263,7 +263,7 @@ func main() {
 		log.Fatalf("Failed to initialize neighbor table: %v", err)
 	}
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 	go func() {

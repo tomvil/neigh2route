19ea2627ebcf2c09c354c95d200fefb815aa0512
Tomas Vilemaitis <tomas.vilemaitis@gmail.com>
2024-10-23 10:22:30 +0300

    add manager tests

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/tomvil/neigh2route
 go 1.21.5
 
 require (
-	github.com/j-keck/arping v1.0.3
+	github.com/go-ping/ping v1.1.0
 	github.com/vishvananda/netlink v1.2.1-beta.2
 )
 
 require (
-	github.com/go-ping/ping v1.1.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
---
+++ internal/neighbor/manager_test.go
@@ -0,0 +1,67 @@
+package neighbor
+
+import (
+	"net"
+	"testing"
+)
+
+// Test NewNeighborManager function
+func TestNewNeighborManager(t *testing.T) {
+	nm, err := NewNeighborManager("lo")
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+
+	if nm.targetInterface != "lo" {
+		t.Errorf("Expected lo, got %s", nm.targetInterface)
+	}
+
+	if nm.targetInterfaceIndex != 1 {
+		t.Errorf("Expected 1, got %d", nm.targetInterfaceIndex)
+	}
+}
+
+func TestNewNeighboerManagerWithInvalidInterface(t *testing.T) {
+	nm, err := NewNeighborManager("invalid")
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+
+	if nm != nil {
+		t.Errorf("Expected nil, got %v", nm)
+	}
+}
+
+func TestAddNeighbor(t *testing.T) {
+	nm, _ := NewNeighborManager("lo")
+
+	ip := net.ParseIP("10.10.10.10")
+	nm.AddNeighbor(ip, 1)
+
+	if len(nm.reachableNeighbors) != 1 {
+		t.Errorf("Expected 1, got %d", len(nm.reachableNeighbors))
+	}
+}
+
+func TestRemoveNeighbor(t *testing.T) {
+	nm, _ := NewNeighborManager("lo")
+
+	ip := net.ParseIP("10.10.10.10")
+	nm.AddNeighbor(ip, 1)
+	nm.RemoveNeighbor(ip, 1)
+
+	if len(nm.reachableNeighbors) != 0 {
+		t.Errorf("Expected 0, got %d", len(nm.reachableNeighbors))
+	}
+}
+
+func TestAddNeighborLinkLocal(t *testing.T) {
+	nm, _ := NewNeighborManager("lo")
+
+	ip := net.ParseIP("fe80::1")
+	nm.AddNeighbor(ip, 1)
+
+	if len(nm.reachableNeighbors) != 0 {
+		t.Errorf("Expected 0, got %d", len(nm.reachableNeighbors))
+	}
+}

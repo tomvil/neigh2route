a07b893d2ee238296f5aae255ff4f835c9862e8f
Tomas Vilemaitis <tomas.vilemaitis@gmail.com>
2024-12-10 14:56:28 +0200

    limit conc runs

--- internal/neighbor/manager.go
+++ internal/neighbor/manager.go
@@ -164,10 +164,14 @@ func (nm *NeighborManager) SendPings() {
 		copy(neighbors, nm.reachableNeighbors)
 		nm.mu.Unlock()
 
+		sem := make(chan struct{}, 5)
 		for _, n := range neighbors {
 			wg.Add(1)
+			sem <- struct{}{}
+
 			go func(n Neighbor) {
 				defer wg.Done()
+				defer func() { <-sem }()
 				if err := netutils.Ping(n.ip.String()); err != nil {
 					log.Printf("Failed to ping neighbor %s: %v", n.ip.String(), err)
 				}
@@ -175,7 +179,7 @@ func (nm *NeighborManager) SendPings() {
 		}
 		wg.Wait()
 
-		<-time.After(30 * time.Second)
+		time.Sleep(30 * time.Second)
 	}
 }
 
@@ -192,7 +196,7 @@ func (nm *NeighborManager) PersistentRoutes() {
 			}
 		}
 
-		<-time.After(30 * time.Second)
+		time.Sleep(30 * time.Second)
 	}
 }
 
